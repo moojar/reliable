710864d77698aad7f8e85ffd6252288250f47169
Jaraad Mootee <jmootee@Jaraads-MacBook-Pro.local>
2014-07-07 01:13:49 -0400

    finished landing page

---
+++ app/header.php
@@ -0,0 +1,9 @@
+        <div class="header"> 
+            <div class='topbar'>
+                <h1>Real Estate Appraisal Hotline : 905 712 8887</h1>
+            </div>
+            <div class='botbar'>
+                <img class="reliable" src='./img/reliable.gif'> 
+                <img class="quality" src='./img/quality.gif'>
+            </div>
+        </div>
